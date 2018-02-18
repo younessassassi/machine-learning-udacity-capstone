c6198d44fd0c38cc01ef5a63739ab35e960e0c48
ASSASSI <ya2257@att.com>
2018-02-18 18:19:17 -0500

    Add sp index to the data

--- processing/processing.py
+++ processing/processing.py
@@ -13,10 +13,12 @@ from common.common import plot_data, visualize_correlation
 
 data_directory = "data/"
 stock_dfs_directory = data_directory + "stock_dfs"
+spy_df_directory = data_directory + "sp_index"
 tickers_pickle = data_directory + "sp500tickers.pickle"
 sp500_joined_closes = data_directory+'sp500_joined_closes.csv'
 sp500_reduced_joined_closes = data_directory+'sp500_reduced_joined_closes.csv'
-sp500_clean_joined_closes = data_directory+'sp500_clean_joined_closes.csv'
+sp_index = spy_df_directory+'/spy.csv'
+sp500_index_reduced = data_directory+'sp500_index_reduced.csv'
 
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -34,10 +36,11 @@ def save_sp500_tickers():
 def getTickerPath(ticker):
     return stock_dfs_directory+'/{}.csv'.format(ticker)
 
-def get_data_from_yahoo(start, end, reload_sp500_ticker=False, update_sp500_data=False):
+def get_data_from_yahoo(start, end, reload_sp500_ticker=False, update_sp500_data=False, update_sp_index=False):
     if start or end:
         # if you change the start or end date, then we need to update the stock data
         update_sp500_data = True
+        update_sp_index = True
     if not start:
         start = dt.datetime(2000, 1, 1) 
     if not end:
@@ -69,6 +72,13 @@ def get_data_from_yahoo(start, end, reload_sp500_ticker=False, update_sp500_data
                 time.sleep(.300) # I added this timeout to ensure that yahoo doesnt cancel my transaction requests 
             else:
                 print('Already have {}'.format(ticker))
+    
+    if not os.path.exists(spy_df_directory):
+        os.makedirs(spy_df_directory)
+
+    if update_sp_index:
+        spDf = pdr.get_data_yahoo("SPY", start, end)
+        spDf.to_csv(sp_index)
 
 
 def create_sp500_joined_closes_file():
@@ -103,21 +113,30 @@ def createReducedDataFrame():
     df = pd.read_csv(sp500_joined_closes)
     df.set_index('Date', inplace=True)
     newDF = df[startDate: endDate]
-    newDF.fillna(method="ffill",inplace="True")
-    newDF.fillna(method="bfill",inplace="True")
+    newDF.fillna(method="ffill",inplace=True)
+    newDF.fillna(method="bfill",inplace=True)
     newDF.to_csv(sp500_reduced_joined_closes)
+
+    spDf = pd.read_csv(sp_index)
+    spDf.set_index('Date', inplace=True)
+    newSpDf = spDf[startDate: endDate]
+    newSpDf.fillna(method="ffill",inplace=True)
+    newSpDf.fillna(method="bfill",inplace=True)
+    newSpDf.to_csv(sp500_index_reduced)
     # visualize_correlation(sp500_reduced_joined_closes)
 
 """Retrieve S&P data"""
 def run():
     update_stock_tickers = False # set to True to refresh the stock tickers used to retrieve stock prices
     update_stock_prices = False # set to True to update the stock prices
+    update_sp500_index_prices = True # set to True to update the S&P500 index
     start_time = None # set to True to update the stock prices
     end_time = None
-    if update_stock_tickers or update_stock_prices or start_time or end_time:   
-        get_data_from_yahoo(start_time, end_time, update_stock_tickers, update_stock_prices)
+    if update_stock_tickers or update_stock_prices or start_time or end_time or update_sp500_index_prices:   
+        get_data_from_yahoo(start_time, end_time, update_stock_tickers, update_stock_prices, update_sp500_index_prices)
         create_sp500_joined_closes_file()
     createReducedDataFrame()
+
 
 if __name__ == "__main__":
     run()
