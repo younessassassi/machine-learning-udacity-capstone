7aa47c11c145c14b0f10b4aa15cad2f1aae3ee94
ASSASSI <ya2257@att.com>
2018-02-27 01:36:54 -0500

    Clean up the processing module

--- processing/start.py
+++ processing/start.py
@@ -11,7 +11,7 @@ import pandas_datareader as pdr
 
 from common.start import plot_data, visualize_correlation, save_sp500_tickers, get_ticker_path, DATA_DIR, STOCK_DF_DIR, TICKERS_PICKLE_DIR
 
-
+"""Retrieve stock information from yahoo finance api"""
 def get_data_from_yahoo(start, end, update_sp500_data=False):
     if start or end:
         # if you change the start or end date, then we need to update the stock data
@@ -48,51 +48,6 @@ def get_data_from_yahoo(start, end, update_sp500_data=False):
                 time.sleep(.100) # I added this timeout to ensure that yahoo doesnt cancel my transaction requests 
             else:
                 print('Already have {}'.format(ticker))
-    
-
-# def create_sp500_joined_closes_file():
-#     if os.path.exists(sp500_joined_closes):
-#         os.remove(sp500_joined_closes)
-
-#     with open(tickers_pickle, "rb") as f:
-#         tickers = pickle.load(f)
-        
-#     main_df = pd.DataFrame()
-    
-#     for count,ticker in enumerate(tickers):
-#         df = pd.read_csv(get_ticker_path(ticker))
-#         df.set_index('Date', inplace=True)
-        
-#         df.rename(columns = {'Adj Close':ticker}, inplace=True)
-#         df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
-        
-#         if main_df.empty:
-#             main_df = df
-#         else:
-#             main_df = main_df.join(df, how='outer')
-            
-#         if count % 10 == 0:
-#             print count
-
-    # main_df.to_csv(sp500_joined_closes)
-
-# def createReducedDataFrame():
-#     startDate = "2012-01-01" 
-#     endDate = "2014-01-01"
-#     df = pd.read_csv(sp500_joined_closes)
-#     df.set_index('Date', inplace=True)
-#     newDF = df[startDate: endDate]
-#     newDF.fillna(method="ffill",inplace=True)
-#     newDF.fillna(method="bfill",inplace=True)
-#     newDF.to_csv(sp500_reduced_joined_closes)
-
-#     spDf = pd.read_csv(sp_index)
-#     spDf.set_index('Date', inplace=True)
-#     newSpDf = spDf[startDate: endDate]
-#     newSpDf.fillna(method="ffill",inplace=True)
-#     newSpDf.fillna(method="bfill",inplace=True)
-#     newSpDf.to_csv(sp500_index_reduced)
-#     # visualize_correlation(sp500_reduced_joined_closes)
 
 """Retrieve S&P data"""
 def run():
