74bc4093e96e2615280a18d4f58d43ba264f398d
ASSASSI <ya2257@att.com>
2018-03-06 21:08:41 -0500

    Add comments to the Ticker class

--- prediction/Ticker.py
+++ prediction/Ticker.py
@@ -1,26 +1,28 @@
 import numpy as np
 
+"""Parent class responsible for initializing the Ticker class with basic pricing data """
 class Ticker(object):
     def __init__(self, symbol=None, data_df={}):
         self.symbol=symbol
         self.df = data_df.rename(columns={symbol: 'Adj Close'})
 
         pass
-
+    
+    """Fill or drop missing data"""
     def _get_clean_df(self):
         df = self.df.copy()
         df.replace('', np.nan, inplace=True)
         df.fillna(method='ffill', inplace=True)
         df.fillna(method='bfill', inplace=True)
         df.dropna(inplace=True)
-        
         return df
 
+    """Return a DataFrame with Ticker adjusted close prices"""
     def get_adj_close_df(self):
         df = self._get_clean_df()
         return df[['Adj Close']]
 
-
+"""Child class with methohds for creating a set of features for a ticker"""
 class TickerAnalysed(Ticker):
     def __init__(self, symbol=None, data_df={}, window=5):
         Ticker.__init__(self, symbol, data_df)
@@ -49,40 +51,48 @@ class TickerAnalysed(Ticker):
         
         pass
     
-
+    """Returns a set of ticker features that can be used for training a model"""
     def get_features(self):
         features = self._get_clean_df()
         features.drop(['Adj Close', 'Next Day Adj Close', 'Moving Average', 'Momentum'], axis=1 , inplace=True)
         return features.values
     
+    """Returns The Ticker label"""
     def get_label(self):
         df = self._get_clean_df()
         label = df['Next Day Adj Close']
         return label
 
+    """Get a dataframe with next day adjusted close price"""
     def get_next_day_df(self):
         df = self._get_clean_df()
         return df[['Next Day Adj Close']]
     
+    """Add next daty adjusted close price"""
     def _add_next_day_price(self, df=None):
         return (df.shift(-1)).rename(columns={'Adj Close': 'Next Day Adj Close'})
-
+    
+    """Print the ticker data as a DataFrame"""
     def print_df(self):
         print self.df  
 
+    """Return a cleaned DataFrame by replacing or remiving missing data"""
     def get_df(self):
           return self._get_clean_df()
     
+    """Calculate the ticker momentum"""
     def _calculate_momentum(self, df=None):
         n=self.momentum_window
         
         df=df/df.shift(n)
         return (df[n:] - 1).rename(columns={'Adj Close': 'Momentum'})
 
+    """Calculate the ticker volatility"""
     def _calculate_volatility(self, df=None):
         n=self.volatility_window
         return ((df.rolling(window=n,center=False).std())[n:] - 1 ).rename(columns={'Adj Close': 'Volatility'})
    
+    """Calculate the ticker Bollinger Bands"""
     def _calculate_bb(self, df=None):
         n=self.bollinger_bands_window
         
@@ -92,11 +102,13 @@ class TickerAnalysed(Ticker):
         lower_bound=ma-2*std
         return upper_bound.rename(columns={'Adj Close': 'BB Upper Bound'}), lower_bound.rename(columns={'Adj Close': 'BB Lower Bound'})
 
+    """Calculate the ticker Simple Moving Average"""
     def _calculate_moving_average(self, df=None):
         n=self.moving_average_window
         
         return ((df/df.rolling(window=n,center=False).mean())[n:] - 1).rename(columns={'Adj Close': 'Moving Average'})
 
+    """Calculate the ticker Daily Returns"""
     def _calculate_daily_returns(self, df=None):
         daily_returns=df.copy()
         daily_returns=(daily_returns/daily_returns.shift(1))-1
